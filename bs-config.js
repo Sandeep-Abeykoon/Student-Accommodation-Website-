68ca61d46fca4081cb588af94609451b1e281cb5
Sandeep-Abeykoon <sandeepchanura@gmail.com>
2024-03-05 09:53:16 +0530

    updated the code

---
+++ bs-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    proxy: 'localhost:3000', // Assuming your PHP server runs on port 8000
+    files: ['**/*.php'], // Watch PHP files for changes
+    ignore: [], // Add any files to ignore
+    reloadDelay: 0, // Delay in milliseconds before reloading
+    ui: false, // Disable BrowserSync UI
+    notify: false // Disable notifications
+  };
+  
