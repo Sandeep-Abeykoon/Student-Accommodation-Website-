b9e1ce00eb30e319ab7b6bd0a93d011dff337304
Sandeep-Abeykoon <sandeepchanura@gmail.com>
2024-03-05 21:53:25 +0530

    updated the code

---
+++ includes/footer.php
@@ -0,0 +1,30 @@
+<footer class="container-fluid bg-white mt-5">
+    <div class="row">
+        <div class="col-lg-4 p-4">
+            <h3 class="h-font fw-bold fs-3 mb-2">NSBM</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Deserunt, corporis odit a possimus, voluptas fugiat tenetur
+                quaerat ex, rerum quos voluptate omnis voluptates aliquam ab
+                consequatur aspernatur repellat debitis libero!</p>
+        </div>
+        <div class="col-lg-4 p-4">
+            <h5 class="mb-3">Links</h5>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a><br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Accomodations</a><br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact Us</a><br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About Us</a>
+        </div>
+        <div class="col-lg-4 p-4">
+            <h5 class="mb-3">Follow Us</h5>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+                <i class="bi bi-twitter me-1"></i>Twitter
+            </a><br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+                <i class="bi bi-facebook me-1"></i>Facebook
+            </a><br>
+            <a href="#" class="d-inline-block text-dark text-decoration-none">
+                <i class="bi bi-instagram me-1"></i>Instagram
+            </a>
+        </div>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 
     .h-font {
       font-family: 'Merienda', cursive;
-    }
+    }s
 
     /* Chrome, Safari, Edge, Opera */
     input::-webkit-outer-spin-button,
@@ -36,54 +36,9 @@
 
 <body class="bg-light">
 
-  <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
-    <div class="container-fluid">
-      <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">NSBM </a>
-      <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active me-2" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link me-2" href="#">Accomodations</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link me-2" href="#">Contact Us</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link me-2" href="#">About Us</a>
-          </li>
+  <!-- Navar -->
+  <?php include 'includes/navbar.php'; ?>
 
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Settings
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-        </ul>
-
-        <div class="d-flex" role="search">
-          <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
-            Login
-          </button>
-          <button type="button" class="btn btn-outline-dark shadow-none" data-bs-toggle="modal" data-bs-target="#registerModal">
-            Register
-          </button>
-        </div>
-      </div>
-    </div>
-  </nav>
 
   <!-- Login Modal -->
   <div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
@@ -220,36 +175,7 @@
   </div>
 
   <!-- Footer -->
-  <div class="container-fluid bg-white mt-5">
-    <div class="row">
-      <div class="col-lg-4 p-4">
-        <h3 class="h-font fw-bold fs-3 mb-2">NSBM</h3>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-          Deserunt, corporis odit a possimus, voluptas fugiat tenetur
-          quaerat ex, rerum quos voluptate omnis voluptates aliquam ab
-          consequatur aspernatur repellat debitis libero!</p>
-      </div>
-      <div class="col-lg-4 p-4">
-        <h5 class="mb-3">Links</h5>
-        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a><br>
-        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Accomodations</a><br>
-        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact Us</a><br>
-        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About Us</a>
-      </div>
-      <div class="col-lg-4 p-4">
-        <h5 class="mb-3">Follow Us</h5>
-        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
-          <i class="bi bi-twitter me-1"></i>Twitter
-        </a><br>
-        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
-          <i class="bi bi-facebook me-1"></i>Facebook
-        </a><br>
-        <a href="#" class="d-inline-block text-dark text-decoration-none">
-          <i class="bi bi-instagram me-1"></i>Instagram
-        </a>
-      </div>
-    </div>
-  </div>
+  <?php include 'includes/footer.php'; ?>
 
   <h6 class="text-center bg-dark text-white p-3 m-0">Designed and Developed By ...</h6>
 
