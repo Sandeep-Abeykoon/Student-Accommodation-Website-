c4265de6f9b7b517542b4239a6e5b96532325576
Sandeep-Abeykoon <sandeepchanura@gmail.com>
2024-03-06 14:26:44 +0530

    Update contactUs.php

--- contactUs.php
+++ contactUs.php
@@ -27,10 +27,18 @@
                 <div class="bg-white rounded shadow p-4">
                     <iframe height="320px" class="w-100 rounded mb-4" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15846.303361252902!2d80.0415729!3d6.8213291!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae2523b05555555%3A0x546c34cd99f6f488!2sNSBM%20Green%20University!5e0!3m2!1sen!2slk!4v1709637123008!5m2!1sen!2slk" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     <h5>Address</h5>
-                    <a href="https://maps.app.goo.gl/BydHW6vmxPG7zmFZA" target="_blank" class="d-inline-block text-decoration-none text-dark mb-2"><i class="bi bi-geo-alt-fill me-1"></i>Pitipana - Thalagala Rd, Homagama</a>
-                    <h5>Call Us</h5>
-          <a href="tel:+94115445000" class="d-inline-block mb-2 text-decoration-none text-dark"><i class="bi bi-telephone-fill"></i> +94 115445000</a>
-          <a href="tel:+94115445000" class="d-inline-block text-decoration-none text-dark"><i class="bi bi-telephone-fill"></i> +94 115445000</a>
+                    <a href="https://maps.app.goo.gl/BydHW6vmxPG7zmFZA" target="_blank" class="d-inline-block text-decoration-none text-dark mb-2"><i class="bi bi-geo-alt-fill"></i> Pitipana - Thalagala Rd, Homagama</a>
+
+                    <h5 class="mt-4">Call Us</h5>
+                    <a href="tel:+94115445000" class="d-inline-block mb-2 text-decoration-none text-dark"><i class="bi bi-telephone-fill"></i> +94 115445000</a><br>
+                    <a href="tel:+94115445000" class="d-inline-block text-decoration-none text-dark"><i class="bi bi-telephone-fill"></i> +94 115445000</a>
+                    <h5 class="mt-4"><i class="bi bi-envelope-at-fill"></i> Email</h5>
+                    <a href="mailto: nsbmaccomodation@gmail.com" class="d-inline-block mb-2 text-decoration-none text-dark">nsbmaccomodation@gmail.com</a>
+
+                    <h5 class="mt-4">Follow Us</h5>
+                    <a href="#" class="d-inline-block text-dark fs-5 me-2"> <i class="bi bi-twitter me-1"></i></a>
+                    <a href="#" class="d-inline-block text-dark fs-5 me-2"> <i class="bi bi-facebook me-1"></i></a>
+                    <a href="#" class="d-inline-block text-dark fs-5"> <i class="bi bi-instagram me-1"></i></a>
                 </div>
             </div>
             <div class="col-lg-6 col-md-6 px-4">
