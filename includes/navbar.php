9bc9e156bc6e5a0253e36d433968920f2b226943
Sandeep-Abeykoon <sandeepchanura@gmail.com>
2024-03-06 08:53:47 +0530

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -45,21 +45,23 @@
 
 <!-- Confirmation dialog modal -->
 <div class="modal" tabindex="-1" role="dialog" id="confirmationModal">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Confirmation</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <p>Are you a landlord?</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#registerModal">Yes</button>
-      </div>
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Confirmation</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+            <span class="badge text-bg-light text-wrap lh-base d-block w-100" style="text-align: left; margin-bottom: 0.5rem;">Note: This registration through the website is only for landlords. If you are related to the university, please contact the university to create your account.</span>
+
+                <p>Are you a landlord?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#registerModal">Yes</button>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
 
 <script>
