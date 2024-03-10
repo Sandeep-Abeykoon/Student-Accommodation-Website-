452c073fe9287d5ccc9e5764635af767a808b2b9
Sandeep-Abeykoon <sandeepchanura@gmail.com>
2024-03-10 13:16:26 +0530

    updated the code

---
+++ admin/settings.php
@@ -0,0 +1,75 @@
+<?php
+include 'essentials.php';
+adminLogin();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel : Settings</title>
+    <?php include 'links.php'; ?>
+</head>
+
+<body class="bg-light">
+
+    <?php include 'header.php'; ?>
+
+    <div class="container-fluid" id="main-content">
+        <div class="row">
+            <div class="col-lg-10 ms-auto p-4 overflow-hidden">
+                <h3 class="mb-4">SETTINGS</h3>
+
+                <!-- General Settings Section -->
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center justify-content-between mb-3">
+                            <h5 class="card-title m-0">General Settings</h5>
+                            <button type="button" class="btn btn-dark shadow-none btn-sm" data-bs-toggle="modal" data-bs-target="#general-settings">
+                                <i class="bi bi-pencil-square"></i>
+                                Edit
+                            </button>
+                        </div>
+                        <h6 class="card-subtitle mb-1 fw-bold">Site Title</h6>
+                        <p class="card-text">content</p>
+                        <h6 class="card-subtitle mb-1 fw-bold">Site About Us</h6>
+                        <p class="card-text">content</p>
+                    </div>
+                </div>
+
+                <!-- Modal -->
+                <div class="modal fade" id="general-settings" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <form>
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title">General Settings</h1>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="mb-3">
+                                        <label class="form-label">Site Title</label>
+                                        <input type="text" name="site-title" class="form-control shadow-none">
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label class="form-label">Address</label>
+                                        <textarea name="site-about" class="form-control shadow-none" rows="5"></textarea>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                                    <button type="button" class="btn btn-primary">Submit</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include 'scripts.php'; ?>
+</body>
+
+</html>
